0745da335e325f1ba4671e799bcc6bd6957d3920
Sam Goto <samuelgoto@gmail.com>
2017-09-11 15:11:27 -0700

    adds a new test for docscript

---
+++ test/docscript.js
@@ -0,0 +1,15 @@
+var fs             = require('fs')
+  , clarinet       = require('../clarinet.js')
+  , npm_stream     = clarinet.createStream()
+  , twitter_stream = clarinet.createStream()
+  , assert         = require('assert')
+  ;
+
+// return;
+
+describe('docscript', function() {
+  describe('basic', function() {
+    it('simple', function() {
+    });
+  });
+});
